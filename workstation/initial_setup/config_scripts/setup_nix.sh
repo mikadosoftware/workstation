e6ca338c31e2797d5699b0ca0a2a8729f30e01d0
Paul Brian <paul@mikadosoftware.com>
2024-12-25 12:43:33 +0000

    updates

--- workstation/initial_setup/config_scripts/setup_nix.sh
+++ workstation/initial_setup/config_scripts/setup_nix.sh
@@ -1,2 +1,15 @@
 #sudo apt install curl
 sh <(curl -L https://nixos.org/nix/install) --daemon
+
+Then we need to 
+https://nix-community.github.io/home-manager/index.xhtml#sec-install-standalone
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+
+nix-channel --update
+
+run this to setup 
+nix-shell '<home-manager>' -A install
+
+add this to ~/.profile
+. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
